388f5e1d9a2ad7e43df70cd33a8c2980a13825bc
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-08 09:06:50 +0100

    make it CLI!

--- index.ts
+++ index.ts
@@ -1,25 +1,22 @@
-import { resolve } from 'node:path';
+#! /usr/bin/env bun
+
+import process from 'node:process';
+import { isAbsolute, resolve } from 'node:path';
 import { cc } from 'bun:ffi';
+import { cli } from 'cleye';
 import { stringToUint8Array } from 'uint8array-extras';
 
 const cmigemoRoot = resolve(__dirname, './koron-cmigemo/');
 const cmigemoSrc = resolve(cmigemoRoot, 'src/');
 
 export const {
-	symbols: {
-		migemoRun,
-		migemoOpen,
-	},
+	symbols: { migemoRun },
 } = cc({
 	source: './main.c',
 	library: ['c', 'migemo'],
 	flags: [`-L${cmigemoRoot}`],
 	include: [cmigemoSrc, cmigemoRoot],
 	symbols: {
-		migemoOpen: {
-			returns: 'int',
-			args: ['buffer'],
-		},
 		migemoRun: {
 			returns: 'int',
 			args: ['buffer', 'buffer'],
@@ -27,9 +24,31 @@ export const {
 	},
 });
 
-const m = migemoOpen(stringToUint8Array(resolve(__dirname, './dict/migemo-compact-dict')));
+const argv = cli({
+	name: 'bun-cmigemo',
 
-migemoRun(
-	stringToUint8Array(resolve(__dirname, './dict/migemo-compact-dict')),
-	stringToUint8Array('nezu'),
-);
+	parameters: [
+		'<query>',
+	],
+
+	flags: {
+		dict: {
+			type: String,
+			alias: 'd',
+			description: 'Path to the migemo dict file',
+			default: './utf-8/migemo-dict',
+		},
+	},
+});
+
+if (import.meta.main) {
+	const { query } = argv._;
+	const { dict } = argv.flags;
+
+	const dictPath = isAbsolute(dict) ? dict : resolve(process.cwd(), dict);
+
+	migemoRun(
+		stringToUint8Array(dictPath),
+		stringToUint8Array(query),
+	);
+}
